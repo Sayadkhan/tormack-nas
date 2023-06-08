c6d502961fbeb4a26249f41fa48cbc465556d5ad
Abdullah Al Sayad <abdullahalsayad03@gmail.com>
2023-06-09 00:03:42 +0600

    static login method added

--- models/user.js
+++ models/user.js
@@ -72,4 +72,25 @@ userSchema.statics.signup = async function (
   return user;
 };
 
+// static login method
+userSchema.statics.login = async function (email, password, ipAddress) {
+  if (!email || !password || !ipAddress) {
+    throw Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ email, ipAddress });
+
+  if (!user) {
+    throw Error("Incorrect email or restriced ip address.");
+  }
+  // compering password
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Incorrect Password");
+  }
+
+  return user;
+};
+
 module.exports = mongoose.model("User", userSchema);
